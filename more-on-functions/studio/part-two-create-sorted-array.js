4598f6630734c9a5d1b29a3ce9f44b904feec7c9
Rupali Pakhale <rupalipakhale@rupalis-mbp.mynetworksettings.com>
2024-02-01 18:03:16 -0500

    array

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,8 +1,9 @@
-function findMinValue(arr){
-  let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
-      min = arr[i];
+
+function findMinValue(nums1){
+  let min = nums1[0];
+  for (i = 0; i < nums1.length; i++){
+    if (nums1[i] < min){
+      min = nums1[i];
     }
   }
   return min;
@@ -19,11 +20,47 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
-
+function sortArray(unsortedArray){
+  let newArray=[];
+  for(let i=0;i<unsortedArray.length;i++){
+    let smallest=findMinValue(unsortedArray);
+    newArray.push(smallest)
+    unsortedArray.splice(unsortedArray.indexOf(smallest),1)
+  }
+  return newArray;
+}
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(sortArray(nums3));
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
+function recursiveSort(unsortedArray,newArray=[]){
+  if (unsortedArray.length--1)
+  {
+    newArray.push(unsortedArray[0])
+  }
+  else
+  {
+    let smallest=findMinValue(unsortedArray);
+    newArray.push(smallest)
+    unsortedArray.splice(unsortedArray.indexOf(smallest),1)
+    recursiveSort(unsortedArray,newArray)
+  }
+  return newArray;
+}
+
+console.log(recursiveSort(nums3));
+
+
+
+
+
+
+
+
+
+
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
