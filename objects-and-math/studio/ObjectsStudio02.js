3e8aecba97dccd6c85d9b895481a4c8ec69bac4e
Rupali Pakhale <rupalipakhale@rupalis-mbp.mynetworksettings.com>
2024-02-04 16:10:01 -0500

    functions

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,15 +1,32 @@
 // Code your orbitCircumference function here:
-
+function orbitCircumference(radius){
+  return Math.round(2*Math.PI*radius)
+}
+console.log(orbitCircumference(2.000));
 
 // Code your missionDuration function here:
-
-
+function missionDuration(orbits,radius=2.000,speed=28.000){
+  let time=(orbits*orbitCircumference(radius))/speed;
+  return Math.round(time*100)/100;
+}
+console.log(missionDuration(5,2.000,28.000));
 // Copy/paste your selectRandomEntry function here:
+let selectRandomEntry=function(ids)
+{
+  let index = Math.floor(Math.random() * ids.length)
+  return ids[index]
+};
 
 
 // Code your oxygenExpended function here:
-
-
+function oxygenExpended(astronaut,orbRad,speed)
+{
+   let duration=missionDuration(3,orbRad,speed);
+  let oxUsed=Math.round(astronaut.o2Used(duration)*1000)/1000;
+  return `${astronaut.name} will perform the spacewalk, which will last ${duration} hours and require ${oxUsed} kg of oxygen.`;
+}
+console.log(oxygenExpended(414,2.000,28.000));
+console.log(`${astronaut.name} will perform the spacewalk, which will last ${duration} hours and require ${oxUsed} kg of oxygen.`);
 // Candidate data & crew array.
 let candidateA = {
    'name':'Gordon Shumway',
@@ -55,4 +72,5 @@ let candidateA = {
  };
  
  let crew = [candidateA,candidateC,candidateE];
+ console.log(selectRandomEntry(crew));
  
